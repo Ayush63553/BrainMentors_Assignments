9c3c6632279fe84a86cb348531f55b70a4241b78
Ayush63553 <116704832+Ayush63553@users.noreply.github.com>
2023-07-27 21:02:06 +0530

    Update assignment_1.js

--- assignment_1.js
+++ assignment_1.js
@@ -1,3 +1,33 @@
+//Question 6
+function prime(a){
+    for(var i=2;i<=a;i++){
+        if(a%i!=0){continue;}
+        else{return "Not Prime";}
+    }
+    if(a==1){return "Not Prime";}
+    return "prime";
+}
+//Question 7
+function arm(a){
+    var b;
+    var temp=a;
+    var sum=0;
+    var count=0;
+    while(temp>0){
+        temp=parseInt(temp/10);
+        count++;
+    }
+    temp=a;
+    for(var i=0;i<=count;i++){
+        b=temp%10;
+        sum=sum+b**count;
+        temp=parseInt(temp/10);
+    }
+    if(sum==a){return true}
+    else{return false}
+}
+//Question 8
+
 function add(){
     var a=0;
     for(var i=0;i<arguments.length;i++){
